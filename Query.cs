38602c0f17a694c0cce5df3900c7da9bd19a6cd0
Jan Hannes Petersen <petersen@willems-daten.de>
2023-06-12 09:02:00 +0200

    Projektdateien hinzufügen.

---
+++ Query.cs
@@ -0,0 +1,24 @@
+﻿using HotChocolate.Types;
+
+namespace TestHC13
+{
+    public class Query
+    {
+        public TestEntity GetTEstEntity() => new TestEntity() { PLZ = "12345", VKNummer = "Test 1234" };
+    }
+
+    public class TestEntityExtension : ObjectTypeExtension<TestEntity> {
+
+        protected override void Configure(IObjectTypeDescriptor<TestEntity> descriptor)
+        {
+            base.Configure(descriptor);
+            
+            // Bug in HC 13.2.1: field names are not overridden, but additionally added.
+            // (is this by design?)
+            descriptor.Field(f => f.PLZ).Name("pLZ");
+            descriptor.Field(f => f.VKNummer).Name("vKNummer");
+        }
+
+    }
+
+}
---
+++ TestEntity.cs
@@ -0,0 +1,13 @@
+﻿using HotChocolate;
+
+namespace TestHC13
+{
+    public class TestEntity
+    {
+        //[GraphQLName("pLZ")]
+        public string? PLZ { get; set; }
+
+        public string? VKNummer { get; set; }
+
+    }
+}
